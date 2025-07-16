b38895aa5bca99cfc9625ebbfead97bf4313ebb3
JesusNazaretGA <jesus.ayala242000@gmail.com>
2025-07-16 15:44:40 -0700

    Set host 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def login():
         mensaje = f'Se guardó el correo {correo} con su contraseña.'
     return render_template('login.html', mensaje=mensaje)
 
-if __name__ == '__main__':
-    crear_tabla_si_no_existe()
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto por variable de entorno
+    app.run(host="0.0.0.0", port=port, debug=True)
+
